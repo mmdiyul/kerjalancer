e0c54627258c442a7defb5d2801b9329237e8f1b
Muhammad Aliyul Murtadlo <muhammadaliyulm@gmail.com>
2018-12-20 08:37:24 +0700

    accept or decline lamaran

--- process/terima-tolak-lamaran-process.php
+++ process/terima-tolak-lamaran-process.php
@@ -1,10 +1,17 @@
 <?php
 include '../lib/connection.php';
 
-if(isset($_POST["accept"])) {
+$idlamaran = $_POST["idlamaran"];
+$idjob = $_POST["idjob"];
 
+if(isset($_POST["accept"])) {
+    $query = "UPDATE applications SET accepted = '1' WHERE id_applications = '$idlamaran'";
+    mysqli_query($con, $query);
+    header("Location: ../client-lihat-pelamar.php?id=$idjob");
 } else if (isset($_POST["batal"])) {
-    
+    $query = "UPDATE applications SET accepted = '0' WHERE id_applications = '$idlamaran'";
+    mysqli_query($con, $query);
+    header("Location: ../client-lihat-pelamar.php?id=$idjob");
 }
 
 
